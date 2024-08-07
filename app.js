cb98edd29a625b30b2a8aedd83ab20ad8912ec93
User <giksona151@gmail.com>
2024-08-08 01:56:33 +0400

    addedsomething

--- app.js
+++ app.js
@@ -32,16 +32,36 @@ $(document).ready(function() {
     });
 
     // Terminal border animations
-    const lines = document.querySelectorAll(".line");
+       function borderAnimationFunction()
+       {
+        const lines = document.querySelectorAll(".line");
 
-    lines.forEach((line, index) => {
-        line.addEventListener("animationend", function() {
-            if (index === lines.length - 1) {
-                lines.forEach(line => line.style.display = 'none');
-                document.getElementById("terminal-container").style.border = "1px solid green";
-                document.querySelector(".terminal--content").style.opacity = 1;
-                document.getElementById("terminal-container").style.borderRadius = "12px";
-            }
+        lines.forEach((line, index) => {
+            line.addEventListener("animationend", function() {
+                if (index === lines.length - 1) {
+                    lines.forEach(line => line.style.display = 'none');
+                    document.getElementById("terminal-container").style.border = "1px solid green";
+                    document.querySelector(".terminal--content").style.opacity = 1;
+                    document.getElementById("terminal-container").style.borderRadius = "12px";
+                }
+            });
         });
-    });
+       }
+
+    // message function
+   function tittleMessage(callback)
+   {
+    let message = document.getElementById("message");
+    message.style.opacity = 1;
+    message.style.direction = "block";
+    setTimeout(() => {
+        message.style.opacity = 0;
+        setTimeout(() => {
+            message.style.display = "none"
+        }, 1000);
+    }, 2500);
+    callback();
+   }
+
+   tittleMessage(borderAnimationFunction)
 });
